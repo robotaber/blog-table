d22d758729c4fcef4f25ff7d980f4220bfa5766d
robotaber <bbgonzalezg20@miumg.edu.gt>
2024-09-28 14:54:38 -0400

    Descripción de los cambios

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const fetchBlogs = async () => {
     setLoading(true);
     try {
-      const response = await fetch('https://api.vercel.app/blogLinks');
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
       const data = await response.json();
       setBlogs(data);
     } catch (error) {
